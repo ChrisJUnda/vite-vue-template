be5971b51ef11e89e41cf09dd051bc6b549722bb
ChrisJUnda <christian.segoviaunda@gmail.com>
2024-07-30 18:05:14 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <h1>
+          app
+        </h1>
+      </div>
+      <div class="col"><a href="">Colonna</a></div>
+      <div class="col"><a href="">Colonna</a></div>
+      <div class="col"><a href="">Colonna</a></div>
+      <div class="col"><a href="">Colonna</a></div>
+      <div class="col"><a href="">Colonna</a></div>
+      <div class="col"><a href="">Colonna</a></div>
+      <div class="col"><a href="">Colonna</a></div>
+      <div class="col"><a href="">Colonna</a></div>
+    </div>
+
+    <ul>
+
+    </ul>
+
+  </div>
+
+
+</template>
+
+<style lang="scss" scoped></style>
